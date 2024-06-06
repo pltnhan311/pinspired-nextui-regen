f2039f8a332e591eae7a0842ade8e8417ce27e2d
pltnhan311 <92429264+pltnhan311@users.noreply.github.com>
2024-06-06 23:03:33 +0700

    update Ui header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,6 +1,5 @@
 import {
   Navbar,
-  NavbarBrand,
   NavbarContent,
   NavbarItem,
   DropdownItem,
@@ -8,7 +7,10 @@ import {
   Dropdown,
   DropdownMenu,
   Avatar,
-  Button
+  Button,
+  NavbarMenu,
+  NavbarMenuItem,
+  NavbarMenuToggle
 } from '@nextui-org/react';
 import { AcmeLogo } from '~/components/acme-logo';
 import { NavLink, useLocation, useNavigate } from 'react-router-dom';
@@ -19,11 +21,16 @@ import toast from 'react-hot-toast';
 import { ThemeSwitcher } from '~/components/theme-provider';
 import { RootState } from '~/store/store';
 import SearchInput from '~/components/search-input';
+import { useState } from 'react';
+
+const menuItems = ['Home', 'Create', 'Message', 'Setting', 'Log Out'];
 
 export default function Header() {
   const navigate = useNavigate();
   const dispatch = useDispatch();
   const location = useLocation();
+
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   const path = location.pathname;
   const user = useSelector((state: RootState) => state.User?.data);
@@ -44,65 +51,80 @@ export default function Header() {
   };
 
   return (
-    <Navbar isBordered>
-      <NavbarContent className='hidden sm:flex gap-4' justify='center'>
-        <NavbarBrand className='cursor-pointer' onClick={() => navigate('/')}>
-          <AcmeLogo />
-          <p className='font-bold text-inherit'>ACME</p>
-        </NavbarBrand>
-        <NavbarItem>
-          <Button as={NavLink} color={`${path === '/' ? 'warning' : 'default'}`} to='/' className='rounded-full'>
-            Home
-          </Button>
-        </NavbarItem>
-        <NavbarItem>
-          <Button
-            as={NavLink}
-            color={`${path === '/create' ? 'warning' : 'default'}`}
-            to='/create'
-            className='rounded-full'
-          >
-            Create
-          </Button>
-        </NavbarItem>
-      </NavbarContent>
+    <div className='flex items-center justify-between'>
+      <Navbar maxWidth='2xl' onMenuOpenChange={setIsMenuOpen} isBordered className='border-default-100'>
+        <NavbarContent>
+          <NavbarMenuToggle aria-label={isMenuOpen ? 'Close menu' : 'Open menu'} className='sm:hidden' />
+          <NavbarItem className='cursor-pointer flex items-center mr-2' onClick={() => navigate('/')}>
+            <AcmeLogo />
+            <p className='font-bold text-inherit'>ACME</p>
+          </NavbarItem>
+          <NavbarItem className='hidden md:flex'>
+            <Button as={NavLink} color={`${path === '/' ? 'warning' : 'default'}`} to='/' className='rounded-full'>
+              Home
+            </Button>
+          </NavbarItem>
+          <NavbarItem className='hidden md:flex'>
+            <Button
+              as={NavLink}
+              color={`${path === '/create' ? 'warning' : 'default'}`}
+              to='/create'
+              className='rounded-full'
+            >
+              Create
+            </Button>
+          </NavbarItem>
+        </NavbarContent>
 
-      <NavbarContent as='div' className='px-5'>
         <SearchInput />
-      </NavbarContent>
 
-      <NavbarContent as='div' className='items-center' justify='end'>
-        <ThemeSwitcher />
-        <Dropdown placement='bottom-end'>
-          <DropdownTrigger>
-            <Avatar
-              isBordered
-              as='button'
-              className='transition-transform'
-              color='secondary'
-              name={user?.FullName}
-              size='sm'
-              src={user?.Avatar}
-            />
-          </DropdownTrigger>
-          <DropdownMenu aria-label='Profile Actions' variant='flat'>
-            <DropdownItem key='profile' className='h-14 gap-2'>
-              <p className='font-semibold'>Signed in as</p>
-              <p className='font-semibold text-yellow-500'>@{user?.UserName}</p>
-            </DropdownItem>
-            <DropdownItem onClick={() => navigate('/profile')} key='posts'>
-              My Wall
-            </DropdownItem>
-            <DropdownItem onClick={() => navigate('/setting')} key='settings'>
-              Settings
-            </DropdownItem>
-            <DropdownItem key='help_and_feedback'>Help & Feedback</DropdownItem>
-            <DropdownItem key='logout' color='danger' onClick={handleLogOut}>
-              Log Out
-            </DropdownItem>
-          </DropdownMenu>
-        </Dropdown>
-      </NavbarContent>
-    </Navbar>
+        <NavbarContent className='hidden md:flex' justify='end'>
+          <ThemeSwitcher />
+          <Dropdown placement='bottom-end'>
+            <DropdownTrigger>
+              <Avatar
+                isBordered
+                as='button'
+                className='transition-transform'
+                color='secondary'
+                name={user?.FullName}
+                size='sm'
+                src={user?.Avatar}
+              />
+            </DropdownTrigger>
+            <DropdownMenu aria-label='Profile Actions' variant='flat'>
+              <DropdownItem key='profile' className='h-14 gap-2'>
+                <p className='font-semibold'>Signed in as</p>
+                <p className='font-semibold text-yellow-500'>@{user?.UserName}</p>
+              </DropdownItem>
+              <DropdownItem onClick={() => navigate('/profile')} key='posts'>
+                My Wall
+              </DropdownItem>
+              <DropdownItem onClick={() => navigate('/setting')} key='settings'>
+                Settings
+              </DropdownItem>
+              <DropdownItem key='help_and_feedback'>Help & Feedback</DropdownItem>
+              <DropdownItem key='logout' color='danger' onClick={handleLogOut}>
+                Log Out
+              </DropdownItem>
+            </DropdownMenu>
+          </Dropdown>
+        </NavbarContent>
+
+        <NavbarMenu>
+          {menuItems.map((item, index) => (
+            <NavbarMenuItem key={`${item}-${index}`}>
+              <NavLink
+                color={index === 2 ? 'primary' : index === menuItems.length - 1 ? 'danger' : 'foreground'}
+                className='w-full'
+                to={item === 'Home' ? '/' : item.toLowerCase()}
+              >
+                {item}
+              </NavLink>
+            </NavbarMenuItem>
+          ))}
+        </NavbarMenu>
+      </Navbar>
+    </div>
   );
 }
--- src/components/search-input.tsx
+++ src/components/search-input.tsx
@@ -5,7 +5,7 @@ const SearchInput = () => {
   return (
     <Input
       classNames={{
-        base: 'max-w-full h-10 w-[20rem] md:min-w-[500px] lg:min-w-[650px]',
+        base: 'max-w-full h-10 flex shrink-1',
         mainWrapper: 'h-full',
         input: 'text-small',
         inputWrapper: 'h-full font-normal text-default-500 bg-default-400/20 dark:bg-default-500/20 rounded-full'
